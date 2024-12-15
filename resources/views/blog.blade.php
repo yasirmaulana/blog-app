3abb58ae058a3718907b44b517c4f27ab13271df
yasirmaulana <yasir.maulana@gmail.com>
2024-12-15 12:45:06 +0700

    first init

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>blog</title>
+</head>
+
+<body>
+    <h1>ini halaman blog</h1>
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi odio voluptatibus amet, numquam iusto et
+            tempora error veniam voluptate. Magni dignissimos aspernatur fugit maxime illo tempore unde dolorum neque
+            quis.</p>
+    </article>
+
+    <article>
+        <h3>judul 2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus veritatis autem sint aperiam animi ullam
+            odio perferendis ad assumenda labore, pariatur tenetur vero perspiciatis impedit voluptatum asperiores
+            voluptas. Autem, voluptatum.</p>
+    </article>
+</body>
+
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>contact</title>
+</head>
+
+<body>
+    <h1>ini halaman contact</h1>
+
+    <ul>
+        <li>
+            Github: <a href="https://github.com/yasirmaulana">@yasirmaulana</a>
+        </li>
+        <li>
+            LinkedIn: <a href="https://linkedin.com/in/yasirmaulana">@yasirmaulana</a>
+        </li>
+    </ul>
+</body>
+
+</html>
